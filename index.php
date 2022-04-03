032919edb937b9b666b9c0c00a5891fc9bca7b14
AalaapHegde <aalaaphegde@gmail.com>
2022-04-03 09:54:50 -0700

    4/3/22

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <title> Insert Chemical Values into Database </title>
+</head>
+<body></body>
+</html>
+<body>
+    <form action = "insert.php" method = "POST">
+        <label> Nitrogen:</label><input type = "text" name = "nitrogen"><br>
+
+        <label> Phosphorus:</label><input type = "text" name = "phosphorus"><br>
+
+        <label> Potassium:</label><input type = "text" name = "potassium"><br>
+
+        <label> Moisture:</label><input type = "text" name = "moisture"><br>
+
+        <label> Time:</label><input type = "text" name = "time"><br>
+
+        <button type = "submit" name = "submit">Submit</button>
+    </form>
+</body>
